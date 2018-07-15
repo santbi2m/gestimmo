04ccbac01ecfeab985deac4b8c3dd6627cdbbf0e
mmb <sangb2m@gmail.com>
2018-07-15 23:32:17 +0200

    MMB Correction du pb d'initialisation automatique des taxes

---
+++ gestimmo-metier/src/main/java/com/ag2m/gestimmo/metier/config/GestimmoContextLoadedListener.java
@@ -0,0 +1,28 @@
+package com.ag2m.gestimmo.metier.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import com.ag2m.gestimmo.metier.service.ParametrageService;
+
+@Component
+public class GestimmoContextLoadedListener implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	ParametrageService parametrageService;
+	
+
+	/**
+	 * Ecoute le chargement du contexte,
+	 * puis initialise les taxes après celui-ci
+	 */
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		parametrageService.loadTaxe();
+		
+	}
+	
+}
