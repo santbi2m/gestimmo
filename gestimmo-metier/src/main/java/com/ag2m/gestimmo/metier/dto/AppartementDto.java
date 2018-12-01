172f4c5950af4546e40216aecfb5b34bf3bbd356
mmb <sangb2m@gmail.com>
2018-12-01 21:53:22 +0100

    MMB Ajout AppartementDto.java

---
+++ gestimmo-metier/src/main/java/com/ag2m/gestimmo/metier/dto/AppartementDto.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.ag2m.gestimmo.metier.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author mombaye
+ *
+ */
+@Getter @Setter
+@ToString(exclude= {"reservations", "anomalies"})
+public class AppartementDto extends IdentifiantDto implements Serializable {
+
+	private static final long serialVersionUID = -7712191825501565673L;
+
+	private String libelle;
+	
+	private String type;
+	
+	private BienDto bien;
+	
+	private Double prix;
+	
+	@JsonIgnore
+	private List<ReservationDto> reservations;
+	
+	@JsonIgnore
+	private List<AnomalieDto> anomalies;
+	
+}
